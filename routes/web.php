3f194007df81d8e07feeaa6a053a92cd20bfdce7
Robzm <172452133+Robzm@users.noreply.github.com>
2025-07-01 18:19:50 -0500

    Add: define route for consulta method in ConsultaController

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ConsultaController;
 
 Route::get('/', function () {
     return view('consultar');
 });
+
+Route::post('/consulta', [ConsultaController::class, 'consulta'])->name('consulta');
+
+
+
 
 
